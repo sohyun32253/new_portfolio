86981f1071cb67769ca675975a7db7131a22cef8
sohyun <sohyun32253@kaywon.ac.kr>
2025-04-23 10:36:08 +0900

    '2'

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import React from 'react'
+
+function App() {
+  return(
+    <div className="bg-blue-500 p-6 text-center text-white">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+export default App;
